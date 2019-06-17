19c30c08735f6e166170a90f4259d1b0fa2c5fb3
diksha mittal <dikshamittal21297@gmail.com>
2019-06-17 22:01:38 +0530

    hgbvu

--- file02.js
+++ file02.js
@@ -1,3 +1,5 @@
 console.log('Hey! we are learning git')
 console.log('use  "git status" to check status')
 console.log('Use "git log" to see history')
+console.log('Use "git log" to see history')
+v
